89fd9f7c44de5e2f103d9f9611b94e75611ef14f
doncream <lamar.mcmiller@gmail.com>
2019-02-15 01:38:00 -0500

    vid 10

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-const Index = () => 
-   <div>
-     Welcome to the Homepage
-   </div>
-  
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Index = () => (
+ <Layout title="Home">
+  <Link href="/about">
+   <a>Go to About page</a>
+  </Link>
+  <p>Welcome to the Homepage</p>
+ </Layout>
+);
 
 export default Index;
