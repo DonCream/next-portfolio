0c68c48a50ce39caa6b8288916b2967802fe6062
doncream <lamar.mcmiller@gmail.com>
2019-02-16 00:45:50 -0500

    done

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const Index = () => (
- <Layout title="Home">
-  <Link href="/about">
-   <a>Go to About page</a>
-  </Link>
-  <p>Welcome to the Homepage</p>
- </Layout>
+  <Layout title="Home">
+    <Link href="/about">
+      <a>Go to About page</a>
+    </Link>
+    <p>Welcome to the Homepage</p>
+  </Layout>
 );
 
 export default Index;
